fdd92fb50b83a712fd106103cbacadb1ffd06081
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-09-18 16:59:55 +0530

    my module

---
+++ impexp/index.js
@@ -0,0 +1,6 @@
+const {add,sub,mult,name} = require('./module');
+
+console.log(add(5,5));
+console.log(sub(5,5));
+console.log(mult(5,5));
+console.log(name);
---
+++ impexp/module.js
@@ -0,0 +1,9 @@
+const add = (a,b)=>a+b;
+
+const sub = (a,b)=>a-b;
+
+const mult = (a,b)=>a*b;
+
+const name = 'YOGESH GAUR';
+
+module.exports = {add,sub,mult,name}
