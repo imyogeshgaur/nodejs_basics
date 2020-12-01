834d9ea3a38babc1c26180d9db3a0865308660dd
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-12-01 18:23:30 +0530

    added the auth middleware

--- REST/loginPage/Router/employeeRoutes.js
+++ REST/loginPage/Router/employeeRoutes.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const Employee = require("../Model/Employee");
 const bcrypt = require("bcrypt");
+const auth = require("../middleware/auth");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   await res.render("index.hbs");
 });
+
 router.get("/index", async (req, res) => {
   await res.render("index.hbs");
 });
+
 router.get("/login", async (req, res) => {
   await res.render("login.hbs");
 });
+
 router.get("/signup", async (req, res) => {
   await res.render("signup.hbs");
+});
+
+router.get("/secret",auth, async (req, res) => {
+  await res.status(200).render("secret.hbs");
 });
 router.post("/signup", async (req, res) => {
   try {
@@ -30,10 +38,10 @@ router.post("/signup", async (req, res) => {
       });
       const token = await newUser.generateToken();
       await newUser.save();
-      res.cookie("jwt",token,{
-        expires:new Date(Date.now()+3000),
-        httpOnly:true
-      })
+      res.cookie("jwtRegister", token, {
+        expires: new Date(Date.now() + 3000),
+        httpOnly: true,
+      });
       res.status(201).render("welcome.hbs");
     } else {
       res.status(403).render("signup.hbs");
@@ -49,7 +57,11 @@ router.post("/login", async (req, res) => {
     const email = req.body.emailOfUser;
     const password = req.body.passwordOfUser;
     const userbyemail = await Employee.findOne({ email });
-    await userbyemail.generateToken();
+    const token = await userbyemail.generateToken();
+    res.cookie("jwtLogin", token, {
+      expires: new Date(Date.now() + 3000),
+      httpOnly: true,
+    });
     if (bcrypt.compare(password, userbyemail.password)) {
       res.status(200).render("welcome.hbs");
     } else {
